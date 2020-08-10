da7f92f29565a808e1c354c71fc3d44631a2b6b6
Callum <callumccr@gmail.com>
2020-08-10 15:22:14 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_appsync_datasource" "default" {
   name             = var.name
   type             = var.type
   description      = var.description
-  service_role_arn = list(var.service_role_arn) > 0 ? [] : element(list(var.service_role_arn), 0)
+  service_role_arn = list(var.service_role_arn) > 0 ? "" : var.service_role_arn
 
   dynamic "dynamodb_config" {
     for_each = var.type == "AMAZON_DYNAMODB" ? var.dynamodb_config : {}

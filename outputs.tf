7850b3197dfdeeb425512934b2edd1d6fae0bd3a
Callum <callumccr@gmail.com>
2020-08-27 12:35:22 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 
 output "dynamodb" {
-  value       = { for d in var.dynamodb_config : d.name => merge(d, { for d in aws_appsync_datasource.dynamodb : arn => d.arn }) }
+  value       = { for d in var.dynamodb_config : d.name => merge(d, { for d in aws_appsync_datasource.dynamodb : "arn" => d.arn }) }
   description = "The ARN of the datasource"
 }
 

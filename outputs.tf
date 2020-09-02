9cd0392361690168b2e40a498d08e7a130eefd6e
Callum <callumccr@gmail.com>
2020-09-02 13:54:31 +0100

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -19,7 +19,7 @@ output "kylin" {
 }
 
 output "kylin_token" {
-  value       = [for d in var.kylin_token_config : value.name]
+  value       = lookup(var.kylin_token_config.id, "")
   description = "The ARN && Name of the datasource"
 }
 

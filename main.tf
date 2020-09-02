a305daa70bf6682dfe70e8d89e45a51e0ca3b72a
Callum <callumccr@gmail.com>
2020-09-02 13:33:31 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "null_resource" "kylin_token" {
                 --name="${lookup(var.kylin_token_config, "name", "")}" \
                 --description="${lookup(var.kylin_token_config, "description", "")}" \
                 --service-role-arn="${lookup(var.kylin_token_config, "service_role_arn", "")}" \
-                --http-config="${file("${path.module}/kylin_token.json")}
+                --http-config="file:////kylin_token.json"
                 EOT
 
   }
